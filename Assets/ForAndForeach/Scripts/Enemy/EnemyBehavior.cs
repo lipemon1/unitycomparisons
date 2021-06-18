78484078d7d3be5407e0b9b3223378519cb8d07b
lipemon1 <ramon_felipe@outlook.com>
2021-06-18 20:46:03 -0300

    Organizing For and Foreach files

---
+++ Assets/ForAndForeach/Scripts/Enemy/EnemyBehavior.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ForAndForeach
+{
+    public class EnemyBehavior : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 _minPos;
+
+        [SerializeField]
+        private Vector3 _maxPos;
+
+        [SerializeField]
+        private float _timeToTakeDamage;
+
+        [SerializeField]
+        private int _minDamage;
+
+        [SerializeField]
+        private int _maxDamage;
+
+        [SerializeField]
+        private Material AliveColor;
+
+        [SerializeField]
+        private Material DeadColor;
+
+        [SerializeField]
+        private MeshRenderer _meshRenderer;
+
+        private Enemy _enemy;
+
+        public void StartEnemy(Enemy enemy)
+        {
+            _enemy = enemy;
+
+            SetPosition();
+            InvokeRepeating(nameof(TryToDie), 0f, _timeToTakeDamage);
+        }
+
+        private void SetPosition()
+        {
+            float posX = Random.Range(_minPos.x, _maxPos.x);
+            float posY = Random.Range(_minPos.y, _maxPos.y);
+            float posZ = Random.Range(_minPos.z, _maxPos.z);
+
+            transform.position = new Vector3(posX, posY, posZ);
+        }
+
+        private void TryToDie()
+        {
+            ChangeColor(_enemy.TakeDamage(Random.Range(_minDamage, _maxDamage)));
+        }
+
+        private void ChangeColor(bool isDead)
+        {
+            if (isDead)
+                _meshRenderer.material = DeadColor;
+            else
+                _meshRenderer.material = AliveColor;
+        }
+
+        public bool IsDead()
+        {
+            return _enemy.IsDead();
+        }
+    }
+}
---
+++ Assets/ForAndForeach/Scripts/Misc/ListCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ForAndForeach
+{
+    public static class ListCreator
+    {
+        public static List<bool> GetBoolList(int sizeWanted)
+        {
+            List<bool> boolList = new List<bool>();
+
+            for (int i = 0; i < sizeWanted; i++)
+            {
+                boolList.Add(Random.Range(0, 1) == 0 ? true : false);
+            }
+
+            return boolList;
+        }
+
+        public static List<Enemy> GetEnemyList(int sizeWanted)
+        {
+            List<Enemy> enemyList = new List<Enemy>();
+
+            for (int i = 0; i < sizeWanted; i++)
+            {
+                enemyList.Add(new Enemy(EnemyManager.GetNewEnemyId()));
+            }
+
+            return enemyList;
+        }
+    }
+}
